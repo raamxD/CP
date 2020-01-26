b8e5d0fe82de1e5ac05203dc0b1e8941bac21e74
unknown <ram22ksh3@gmail.com>
2020-01-26 23:55:42 +0530

    Initial commit

---
+++ Range Query/Segment Tree/Iterative Segment Tree/RangeIncrement_MinMaxQueries.cpp
@@ -0,0 +1,58 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+struct SegLzy{
+	typedef long long ll;
+    #define _  j*2
+    #define __ j*2+1
+	
+    vector<int> S,L; ll n,h,tt;
+    //pass 't' as {-1,1} for {min,max} segtree
+    SegLzy(vector<int> ar,int t){
+        n=ar.size(); h=32-__builtin_clz(n); tt=-1e18*t;
+        S.assign(2*n,0); L.assign(n,0);
+        for(int i=0;i<n;i++)S[n+i]=ar[i];
+        for(int j=n-1;j>0;j--)S[j]=fn(S[_],S[__]);  
+    }
+	void inline Push(long long p){
+        for(int i=h,j=p>>i;i>0;--i,j=p>>i)if(L[j]){
+            S[_] +=L[j];if(_ <n)L[_] +=L[j];
+            S[__]+=L[j];if(__<n)L[__]+=L[j];
+            L[j]=0;
+        } 
+    }
+    void Updt(int l,int r,int v){
+        ll s=l+n,e=r-1+n,j;
+        for(l+=n,r+=n;l<r;l/=2,r/=2){
+            if(l&1){S[l]+=v;if(l<n)L[l]+=v;l++;}
+            if(r&1){--r;S[r]+=v;if(r<n)L[r]+=v;}
+        }
+        j=s;while((j/=2)>0)S[j]=fn(S[_],S[__])+L[j];
+        j=e;while((j/=2)>0)S[j]=fn(S[_],S[__])+L[j];
+    }
+    long long Kqry(int l,int r){
+        Push(l+n); Push(r-1+n); ll res=tt;
+        for(l+=n,r+=n;l<r;l/=2,r/=2){
+            if(l&1)res=fn(res,S[l++]);
+            if(r&1)res=fn(res,S[--r]);
+        }return res;
+    }
+    inline ll fn(ll x,ll y){
+    	return(tt<0)?max(x,y):min(x,y);
+    }
+};
+
+int main() {
+    
+    vector<int> a = {1,2,3,4,5,6,7,18,9,10};
+    SegLzy S(a,1);
+    cout << "Kqry [0,10) : " << S.Kqry(0,10) << endl;
+    cout << "Kqry [0,5)  : " << S.Kqry(0,5) << endl;
+    S.Updt(3,7,10);
+    cout << "Kqry [0,10) : " << S.Kqry(0,10) << endl;
+    cout << "Kqry [0,5)  : " << S.Kqry(0,5) << endl;
+
+    return 0;
+}
+
+//-----------------------------------------------------------------------------					
---
+++ Range Query/Segment Tree/Iterative Segment Tree/RangeIncrement_SumQueries.cpp
@@ -0,0 +1,129 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+struct SegLzy{
+    typedef long long ll;
+    #define _  j*2
+    #define __ j*2+1
+
+    vector<ll> S,L; int n,h;
+
+    SegLzy(vector<int> ar){
+        n=ar.size(); h=32-__builtin_clz(n);
+        S.assign(2*n,0); L.assign(n,0);
+        for(int i=0;i<n;i++)S[n+i]=ar[i];
+        for(int j=n-1;j>0;j--)S[j]=S[_]+S[__];  
+    }
+    inline void Push(int l,int r) {
+        ll s=h,k=1<<(h-1);
+        for(l+=n,r+=n-1;s>0;--s,k/=2){
+            for(int j=l>>s;j<=r>>s;++j)if(L[j]){
+                S[_] +=L[j]*k;if(_ <n)L[_] +=L[j];
+                S[__]+=L[j]*k;if(__<n)L[__]+=L[j];
+                L[j] = 0;
+            }
+        }      
+    }
+    void Updt(int l,int r,int v){
+        ll s=l+n,e=r-1+n,k=1,j,c;
+        for(l+=n,r+=n;l<r;l/=2,r/=2,k*=2) {
+            if(l&1){S[l]+=v*k;if(l<n)L[l]+=v;l++;}
+            if(r&1){--r;S[r]+=v*k;if(r<n)L[r]+=v;}
+        }
+        j=s;c=1;while((j/=2)>0)c*=2,S[j]=S[_]+S[__]+L[j]*c;
+        j=e;c=1;while((j/=2)>0)c*=2,S[j]=S[_]+S[__]+L[j]*c;
+    }
+    long long Kqry(int l,int r,ll res=0){
+        Push(l,l+1);Push(r-1,r);
+        for(l+=n,r+=n;l<r;l/=2,r/=2){
+            if(l&1)res+=S[l++];
+            if(r&1)res+=S[--r];
+        }return res;
+    }
+};
+
+int main(){
+    vector<int> a = {1,2,3,4,5,6,7,8,9,10};
+    SegLzy S1(a);
+    cout << S1.Kqry(0,10) << endl;
+    S1.Updt(3,7,10);
+    cout << S1.Kqry(3,7) << endl;
+    S1.Updt(3,7,-10);
+    cout << S1.Kqry(3,7) << endl;
+}
+
+//------------------------------------------------------------------------
+
+// struct SegLzy{
+//     typedef long long ll;
+//     vector<int> S,L; ll n,h;
+
+//     SegLzy(vector<int> ar){
+//         n=ar.size(); h=32-__builtin_clz(n);
+//         S.assign(2*n,0); L.assign(n,0);
+//         for(int i=0;i<n;i++)S[n+i]=ar[i];
+//         for(int j=n-1;j>0;j--)S[j]=S[2*j]+S[2*j+1];  
+//     }
+
+//     void calc(int p, int k) {
+//         S[p] = S[p<<1] + S[p<<1|1] + L[p] * k;
+//     }
+    
+//     void apply(int p, int value, int k) {
+//         S[p] += value * k;
+//         if (p < n) L[p] = value;
+//     }
+
+//     void build(int l, int r) {
+//         int k = 2;
+//         for (l += n, r += n-1; l > 1; k <<= 1) {
+//             l >>= 1, r >>= 1;
+//             for (int i = r; i >= l; --i) calc(i, k);
+//         }
+//     }
+
+//     void push(int l, int r) {
+//         int s = h, k = 1 << (h-1);
+//         for (l += n, r += n-1; s > 0; --s, k >>= 1)
+//             for (int i = l >> s; i <= r >> s; ++i) if (L[i] != 0) {
+//                 apply(i<<1, L[i], k);
+//                 apply(i<<1|1, L[i], k);
+//                 L[i] = 0;
+//             }
+//     }
+
+//     void modify(int l, int r, int value) {
+//         if (value == 0) return;
+//         push(l, l + 1);
+//         push(r - 1, r);
+//         int l0 = l, r0 = r, k = 1;
+//         for (l += n, r += n; l < r; l >>= 1, r >>= 1, k <<= 1) {
+//             if (l&1) apply(l++, value, k);
+//             if (r&1) apply(--r, value, k);
+//         }
+//         build(l0, l0 + 1);
+//         build(r0 - 1, r0);
+//     }
+
+//     int query(int l, int r) {
+//         push(l, l + 1);
+//         push(r - 1, r);
+//         int res = 0;
+//         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+//             if (l&1) res += S[l++];
+//             if (r&1) res += S[--r];
+//         }
+//         return res;
+//     }
+// };
+
+// int main(){
+//     vector<int> a = {1,2,3,4,5,6,7,8,9,10};
+//     SegLzy S1(a);
+//     cout << S1.query(0,10) << endl;
+//     S1.modify(3,7,10);
+//     cout << S1.query(3,7) << endl;
+//     S1.modify(3,7,-10);
+//     cout << S1.query(3,7) << endl;
+// }
+
