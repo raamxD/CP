f8ba3f0b535ea879ac799b5cf588eee6d4d7fbb3
unknown <ram22ksh3@gmail.com>
2020-03-25 23:22:15 +0530

    Initial Commit

---
+++ String/RabinKarpforMatching.cpp
@@ -0,0 +1,62 @@
+/*  
+ *  Source to study :  https://cp-algorithms.com/string/rabin-karp.html 
+ *  Aim : Given 2 strings 't' and 's', count occurrences of string 's'
+ *        in string 't'.
+ *  Time Complexity : O(|s| + |t|)
+ */
+#define _GLIBCXX_DEBUG
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+struct RabinKarp{
+    
+    const int p = 31;
+    const int m = 1000000007;
+    vector<long long> poww;
+
+    RabinKarp(int mxN){
+        poww.assign(mxN,0);
+        poww[0] = 1;
+        for(int i = 1; i < mxN; ++i)
+            poww[i] = (poww[i - 1] * p) % m;
+    }
+    //returns occurrences of 's' in 't'
+    vector<int> getCount(string const &s, string const &t){
+        int S = s.size();
+        int T = t.size();
+        vector<long long> h(T + 1, 0);
+        for(int i = 0; i < T; ++i)
+            h[i + 1] = (h[i] + (t[i] - 'a' + 1) * poww[i]) % m;
+        long long hs = 0;
+        for(int i = 0; i < S; ++i)
+            hs = (hs + (s[i] - 'a' + 1) * poww[i]) % m;
+        vector<int> occ;
+        for(int i = 0; i + S - 1 < T; ++i){
+            long long curh = (h[i + S] + m - h[i]) % m;
+            if(curh == (hs * poww[i]) % m)
+                occ.push_back(i);
+        }
+        return occ;
+    }   
+};
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string t = "abcdfbcdef";
+    string s = "bcd";
+
+    RabinKarp RK(100);
+    vector<int> occ = RK.getCount(s,t);
+    if(!occ.empty())
+        cout << "string 't' appears at pos : ";
+    for(auto i : occ)
+        cout << i + 1 << " ";
+    cout << "\n";
+}
+
+
+
---
+++ String/StringHash.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+struct hashFunc{
+    
+    const int p = 31;
+    const int m = 1000000007;
+    vector<long long> poww;
+
+    hashFunc(int mxN){
+        poww.assign(mxN,1);
+        for(int i = 1; i < mxN; ++i){
+            poww[i] = (poww[i - 1] * p) % m;
+        }
+    }
+    long long hashVal(string const &s){
+        int res = 0;
+        int idx = 0;
+        for(auto i : s){
+            res += ((i - 'a' + 1) * poww[idx++]) % m;
+        }
+        return res;
+    }
+};
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string s1 = "bitch";
+    string s2 = "bitch";
+
+    hashFunc H(100);
+    cout << H.hashVal(s1) << " " << H.hashVal(s2) << "\n";
+}
